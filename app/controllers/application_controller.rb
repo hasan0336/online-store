75bc1a8930a9a6c2cc01e3d44dc8046cfab1e835
hasan0336 <hasan_naqvi10@hotmail.com>
2018-03-13 13:16:39 -0700

    add name field in form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+  	# we have to use name field in two forms one in sign_up and second in account_update form
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  	devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
