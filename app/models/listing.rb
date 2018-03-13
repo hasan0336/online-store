78ab7f288360ebb613eb1b88bcae6ff96bdfa70a
hasan0336 <hasan_naqvi10@hotmail.com>
2018-03-13 13:58:09 -0700

    validation in form

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,9 @@ class Listing < ApplicationRecord
 	storage: :cloudinary,
   :path => ':id/:style/:filename'
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+	validates :name, :description, :price , presence: true
+	# checks if price is a number and it is greater than 0
+	validates :price, numericality: { greater_than: 0 }
+	validates_attachment_presence :image
 end
